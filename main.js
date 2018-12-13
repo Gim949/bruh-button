ff85325223afb8b76098fd88460bbf1eefbab5f4
Parker St. Pierre <parker@saint.moe>
2018-12-12 18:42:37 -0800

    Added clicks per minute counter; used bootstrap to center all elements

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-var clicks = 0;
-    function onClick() {
-        var bruhAudio = new Audio("bruh.mp3");
-        bruhAudio.play()
-        clicks += 1;
-        document.getElementById("total-clicks").innerHTML = clicks;
-    };
+var cps = document.getElementById("cps");
+var totalClicks = document.getElementById("total-clicks");
+var count = 0;
+var start = 0;
+function onClick() {
+    var bruhAudio = new Audio("bruh.mp3");
+    bruhAudio.play()
+    count++;
+    start++;
+    document.getElementById("total-clicks").innerHTML = start;
+};
+getCPS();
+function getCPS() {
+  setTimeout(function() {
+    document.getElementById("cps").innerHTML = count;
+    count = 0;
+    getCPS();
+  }, 1000);
+}
