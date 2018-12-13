05c0cfe654a95fbc2c499eeb66bd738f4d2acd91
Parker St. Pierre <parker@saint.moe>
2018-12-12 16:55:49 -0800

    Updated button image; added total clicks counter; added bootstrap

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-function bruh() {
-    var bruhAudio = new Audio("bruh.mp3");
-    bruhAudio.play();
-};
+var clicks = 0;
+    function onClick() {
+        var bruhAudio = new Audio("bruh.mp3");
+        bruhAudio.play()
+        clicks += 1;
+        document.getElementById("total-clicks").innerHTML = clicks;
+    };
