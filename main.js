db014cf12ef672448fcfe4fc6790e0daa914ac39
Parker St. Pierre <parker@saint.moe>
2018-12-13 18:02:41 -0800

    added favicon; made tab space the same everywhere

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
 if (getCookie("totalClicks") == "") {
-  var totalClicks = 0;
+    var totalClicks = 0;
 } else {
-  var totalClicks = getCookie("totalClicks");
+    var totalClicks = getCookie("totalClicks");
 }
 document.getElementById("total-clicks").innerHTML = totalClicks;
 var cps = document.getElementById("cps");
 var currentClicks = 0;
 getCPS();
 function onClick() {
-  playbruh();
-  updateClicks();
+    playbruh();
+    updateClicks();
 }
 function playbruh(name) {
-  var bruhAudio = new Audio("/resources/bruh.mp3");
-  bruhAudio.play();
+    var bruhAudio = new Audio("/resources/bruh.mp3");
+    bruhAudio.play();
 }
 function updateClicks() {
-  currentClicks++;
-  totalClicks++;
-  document.getElementById("total-clicks").innerHTML = totalClicks;
-  document.cookie = "totalClicks=" + totalClicks;
+    currentClicks++;
+    totalClicks++;
+    document.getElementById("total-clicks").innerHTML = totalClicks;
+    document.cookie = "totalClicks=" + totalClicks;
 }
 function getCPS() {
-  setTimeout(function() {
-    document.getElementById("cps").innerHTML = currentClicks;
-    currentClicks = 0;
-    getCPS();
-  }, 1000);
+    setTimeout(function() {
+        document.getElementById("cps").innerHTML = currentClicks;
+        currentClicks = 0;
+        getCPS();
+    }, 1000);
 }
 function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
 }
