1b49c279296b3915787b8fc8b4c92e591d9df117
Parker St. Pierre <parker@saint.moe>
2018-12-13 16:38:26 -0800

    Cleaned up code and directories; updated button image

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
-var cps = document.getElementById('cps');
-if(getCookie('totalClicks') == '') {
+if (getCookie("totalClicks") == "") {
   var totalClicks = 0;
-  document.getElementById('total-clicks').innerHTML = 0;
 } else {
-  var totalClicks = getCookie('totalClicks');
-  document.getElementById('total-clicks').innerHTML = totalClicks;
+  var totalClicks = getCookie("totalClicks");
 }
+document.getElementById("total-clicks").innerHTML = totalClicks;
+var cps = document.getElementById("cps");
 var currentClicks = 0;
 getCPS();
 function onClick() {
-  var bruhAudio = new Audio('bruh.mp3');
-  bruhAudio.play()
+  playbruh();
+  updateClicks();
+}
+function playbruh(name) {
+  var bruhAudio = new Audio("/resources/bruh.mp3");
+  bruhAudio.play();
+}
+function updateClicks() {
   currentClicks++;
   totalClicks++;
-  document.getElementById('total-clicks').innerHTML = totalClicks;
-  document.cookie = 'totalClicks=' + totalClicks;
+  document.getElementById("total-clicks").innerHTML = totalClicks;
+  document.cookie = "totalClicks=" + totalClicks;
 }
 function getCPS() {
   setTimeout(function() {
-    document.getElementById('cps').innerHTML = currentClicks;
+    document.getElementById("cps").innerHTML = currentClicks;
     currentClicks = 0;
     getCPS();
   }, 1000);
 }
 function getCookie(cname) {
-  var name = cname + '=';
+  var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
+  var ca = decodedCookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
       return c.substring(name.length, c.length);
     }
   }
-  return '';
+  return "";
 }
